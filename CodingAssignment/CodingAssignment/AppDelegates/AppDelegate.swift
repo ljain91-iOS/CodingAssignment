f464c40db5169421e1c806611c423786482d73e6
Lokesh Jain <jain.lokesh0810@gmail.com>
2020-07-21 19:18:38 +0530

    Added and configured reachability manager, network error toast

--- CodingAssignment/CodingAssignment/AppDelegates/AppDelegate.swift
+++ CodingAssignment/CodingAssignment/AppDelegates/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
+    // Setup reachability
+    ReachabilityManager.sharedManager.setupReachability()
+    
     // Check iOS before configuring UIWindow
     if #available(iOS 13.0, *) {
       // Set rootViewController in SceneDelegate
